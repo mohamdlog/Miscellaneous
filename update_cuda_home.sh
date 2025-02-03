61b91547eb9293b41271fda3874c3890e932acb0
Mohammad Al-Hanoosh <127714779+mohamdlog@users.noreply.github.com>
2025-02-02 22:52:45 -0600

    Update update_cuda_home.sh

--- update_cuda_home.sh
+++ update_cuda_home.sh
@@ -2,6 +2,8 @@
 update_cuda_home() {
     if [[ -n "$CONDA_PREFIX" ]]; then
         export CUDA_HOME="$CONDA_PREFIX"
+        export PATH=$CONDA_PREFIX/bin:$PATH
+        export LD_LIBRARY_PATH="$CUDA_HOME/lib:$LD_LIBRARY_PATH"
     else
         unset CUDA_HOME  # Unset when no environment is active
     fi
